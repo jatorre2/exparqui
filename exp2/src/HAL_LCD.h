a2208614d9d91324458e5e2c39bd57c3b4a0e4c8
Jorge Torres <ocklurvox@gmail.com>
2013-10-01 15:17:36 -0400

    Exp 2

---
+++ exp2/src/HAL_LCD.h
@@ -0,0 +1,16 @@
+#include <stdint.h>
+
+void LCD_init(void);
+void LCD_backlightInit(void);
+void LCD_writeCommand(uint8_t* sCmd, uint8_t i);
+void LCD_setAllPixels(uint8_t set);
+void LCD_writeData(uint8_t data);
+void LCD_clear(void);
+void LCD_setPixel(uint8_t i, uint8_t j,uint8_t set);
+void LCD_cleanpage(uint8_t page);
+int LCD_putchar(int character);
+int16_t LCD_printf(const char * fmt, ...);
+void LCD_draw_img();
+void LCD_adjust_contrast(uint8_t set);
+void LCD_reset_address();
+void LCD_println(const char * st);
---
+++ exp2/src/test.h
@@ -0,0 +1,6 @@
+#include <stdint.h>
+#include "msp430.h"
+#include "HAL_LCD.h"
+
+void wait_for_button_and_clear(void);
+void test(void);
