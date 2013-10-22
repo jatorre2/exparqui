350a13bf3186af72160be84667e5c0b974d10715
Felipe Cortés <fcsfelipe@gmail.com>
2013-10-22 15:37:06 -0300

    oli

---
+++ exp4/src/accel.c
@@ -0,0 +1,19 @@
+#include <msp430.h>
+
+
+//Pines
+#define ACCEL_CS BIT5 //CS P3.5
+#define ACCEL_SIMO BIT3 //SIMO P3.3
+#define ACCEL_INT BIT5 //INT P2.5
+#define ACCEL_SCK BIT7 //SCK P2.7
+#define ACCEL_PWR BIT6 //PWR P3.6
+#define ACCEL_SOMI BIT4 //SOMI P3.4
+
+
+//Puertos
+
+void accel_init() {
+
+    //Power on
+
+}
