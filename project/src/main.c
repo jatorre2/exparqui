d5cbdf4b9ed710f810112ac66528ce18093f353a
Jorge Torres <ocklurvox@gmail.com>
2013-11-12 15:46:41 -0300

    raspberry a msp

--- project/src/main.c
+++ project/src/main.c
@@ -2,6 +2,7 @@
 #include <stdint.h>
 #include <string.h>
 #include <msp430.h>
+#include "HAL_LCD.h"
 #include "uart.h"
 
 #define DEBUG(...) do {uart_printf(__VA_ARGS__);} while(0);
@@ -17,15 +18,20 @@ void init() {
 	led_init();
 	uart_init();
 	__enable_interrupt();
+        LCD_init();
+	LCD_backlightInit();
 }
+
+extern volatile uint8_t flag_uart;
 
 int main (void)
 {
 	init();
+	LCD_clear();
 	uint8_t n = 0;
 	uint8_t c = 1;
 	while(1) {
-		if(c==1) {
+		/*if(c==1) {
 			n=n+1;
 			if(n > 60) {
 				c=0;
@@ -38,7 +44,12 @@ int main (void)
 		}
 		for(int i = 0; i < n; i++)
 			DEBUG(".");
-		DEBUG("\n\r");
+		DEBUG("\n\r");*/
+		if(flag_uart){
+
+			LCD_printf("%c",UCA1RXBUF);
+			flag_uart = 0;
+		}
 	}
 	return 0;
 }
